db2c803902a682d80bbdfcdf3d7dd6dfc0152ce7
Kriventsova <39691222+Kriventsova@users.noreply.github.com>
2021-12-15 03:16:21 +0300

    Create Launcher.java

---
+++ lab7/Launcher.java
@@ -0,0 +1,14 @@
+
+
+import lab.task2.gui.MainWindow;
+
+import javax.swing.*;
+
+public class Launcher {
+    public final static String splitter = "\\";
+
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new MainWindow();
+    }
+}
